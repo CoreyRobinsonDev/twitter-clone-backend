e1846e5cc57da0a096c28bbd40eb5ed3b4794fb1
CoreyRobinsonDev <coreyrob52@gmail.com>
2022-05-26 23:16:48 -0400

    saved dummy data to json

--- index.js
+++ index.js
@@ -1,13 +1,3 @@
 const axios = require("axios");
 const fs = require("fs");
-
-let data;
-
-axios.get("https://www.reddit.com/r/all.json")
-  .then((response) => {
-
-    // fs.writeFile("data.json", JSON.stringify(response.data), function(err) {
-    //   if (err) return console.log(err);
-    // })
-  })
-
+const data = require("./data.json");

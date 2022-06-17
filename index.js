774cd6e615573d497223e48d43a4c41f647e2dfd
CoreyRobinsonDev <coreyrob52@gmail.com>
2022-06-17 12:25:56 -0400

    created likes and bookmarks tables. recreated the posts table to include time

--- index.js
+++ index.js
@@ -1,20 +1,28 @@
+const express = require("express");
+const app = express();
 const sqlite3 = require("sqlite3").verbose();
-const db = new sqlite3.Database("./database/bitter.db", sqlite3.OPEN_READWRITE, (err) => {
-  if (err) return console.error(err.message)
-  console.log("Connection Successful")
-})
+const cors = require("cors");
+
+app.use(cors())
 
 
+const PORT = 4001;
 
-const main = async () => { 
 
-  db.all("SELECT username FROM users", [], (err, data) => {
-    console.log(data)
+app.get("/", (req, res) => {
+  const db = new sqlite3.Database("./database/bitter.db", sqlite3.OPEN_READWRITE, (err) => {
+    if (err) return console.error(err.message);
+  })
+  
+  db.all("SELECT text, media, date_post_created, num_comments, num_upvotes, num_downvotes, num_reposts, username, profile_photo FROM posts JOIN users ON users.id = posts.poster_id LIMIT 30", [], (err, rows) => {
+    if (err) return res.status(500).json(err);
+
+    res.send(rows)
   })
 
   db.close((err) => {
     if (err) return console.error(err)
   })
-}  
+})
 
-main();
+app.listen(PORT, () => console.log(`Server listening on port ${PORT}`))
--- util/helper.js
+++ util/helper.js
@@ -15,6 +15,11 @@ const generate_password_hash = async (password, salt_rounds = 10) => {
 exports.generate_password_hash = generate_password_hash;
 
 exports.generate_user = async () => {
+  const sqlite3 = require("sqlite3").verbose();
+  const db = new sqlite3.Database("./database/bitter.db", sqlite3.OPEN_READWRITE, (err) => {
+    if (err) return console.error(err.message)
+  })
+
    await axios.get("https://random-data-api.com/api/users/random_user")
   .then(response => {
     const { username, password, avatar } = response.data;
@@ -27,5 +32,47 @@ exports.generate_user = async () => {
   })
   .catch(error => {
     if (error) return console.error(error)
+  })
+
+  db.close((err) => {
+    if (err) return console.error(err)
+  })
+}
+
+
+exports.generate_post = async () => {
+  const sqlite3 = require("sqlite3").verbose();
+  const db = new sqlite3.Database("./database/bitter.db", sqlite3.OPEN_READWRITE, (err) => {
+    if (err) return console.error(err.message)
+  })
+  const now = new Date();
+  let post = {time: Math.floor(now / 1000)};
+
+  await axios.get("https://random-data-api.com/api/lorem_ipsum/random_lorem_ipsum")
+    .then(response => {
+      const { very_long_sentence, short_sentence, question, word } = response.data;
+      const text_arr = [very_long_sentence, short_sentence, question, word, ""];
+      post.text = text_arr[Math.floor(Math.random() * text_arr.length)];
+    })
+    .catch(error => {
+      if (error) return console.error(error);
+    })
+  await axios.get("https://random-data-api.com/api/lorem_flickr/random_lorem_flickr")
+    .then(response => {
+      const { image, image_50_60, image_500_500, image_1920_1080 } = response.data;
+      const img_arr = [image, image_1920_1080, image_500_500, image_50_60, ""];
+
+      post.media = img_arr[Math.floor(Math.random() * img_arr.length)];
+    })
+    .catch(error => {
+      if (error) return console.error(error);
+    })
+  
+  db.run("INSERT INTO posts (poster_id, text, media, num_comments, num_upvotes, num_downvotes, num_reposts, date_post_created) VALUES(?, ?, ?, ?, ?, ?, ?, ?)", [Math.floor(Math.random() * 1000), post.text, post.media, 0, 0, 0, 0, post.time], err => {
+    if (err) return console.error(err);
+  })
+
+  db.close((err) => {
+    if (err) return console.error(err)
   })
 }

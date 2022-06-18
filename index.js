d4a901bdb45154435eddf909a450c81b062ffe79
CoreyRobinsonDev <coreyrob52@gmail.com>
2022-06-18 14:25:52 -0400

    added sessions and user auth, along with their routes

--- index.js
+++ index.js
@@ -2,13 +2,30 @@ const express = require("express");
 const app = express();
 const sqlite3 = require("sqlite3").verbose();
 const cors = require("cors");
-
-app.use(cors())
-
+const passport = require("passport");
+const cookieParser = require("cookie-parser");
+const bcrypt = require("bcrypt");
+const session = require("express-session");
+const bodyParser = require("body-parser");
 
 const PORT = 4001;
 
+// Middleware
+app.use(cors({origin: "http://localhost:3000", credentials: true}))
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(session({
+  secret: "secretcode",
+  resave: true,
+  saveUninitialized: true
+}));
+app.use(cookieParser("secretcode"));
+app.use(passport.initialize());
+app.use(passport.session());
+require("./util/passportConfig")(passport);
 
+
+// Routes
 app.get("/", (req, res) => {
   const db = new sqlite3.Database("./database/bitter.db", sqlite3.OPEN_READWRITE, (err) => {
     if (err) return console.error(err.message);
@@ -24,5 +41,68 @@ app.get("/", (req, res) => {
     if (err) return console.error(err)
   })
 })
+
+
+app.post("/register", async (req, res) => {
+  const { password, confirmPassword } = req.body;
+  const username = req.body.username.trim().toUpperCase();
+  const db = new sqlite3.Database("./database/bitter.db", sqlite3.OPEN_READWRITE, (err) => {
+    if (err) return console.error(err.message);
+  })
+
+  db.all("SELECT * FROM users WHERE username = ?", [username], (err, row) => {
+    if (err) console.log(err);
+    if (err) return res.status(500).send("Server Error");
+    if (row.length > 0) return res.status(404).send("User Already Exists");
+  })
+
+  if (password !== confirmPassword) return res.status(403).send("Password doesn't Match Confirmed Password")
+
+  const hash = await bcrypt.hash(password, 10);
+  const date = new Date().toLocaleDateString("en-US");
+  
+  db.run("INSERT INTO users (username, password, num_tweets, num_followers, num_following, date_acc_created) VALUES(?, ?, ?, ?, ?, ?)", [username, hash, 0, 0, 0, date], (err) => {
+    if (err) {
+      console.log(err);
+      return res.status(500).send("Server Error");
+    }
+
+    res.status(201).send("Account Created");
+  })
+
+  db.close((err) => {
+    if (err) return console.error(err)
+  })
+})
+
+
+app.post("/login", (req, res, next) => {
+  passport.authenticate("local", (err, user, info) => {
+    if (err) throw err;
+    if (!user) {
+      res.status(404).send("No User was Found");
+    } else {
+      req.logIn(user, (err) => {
+        if (err) throw err;
+        res.status(200).send("Successful");
+        console.log(req.user);
+      })
+    }
+
+  })(req, res, next)
+})
+
+
+app.get("/logout", (req, res) => {
+  req.logout();
+})
+
+
+app.get("/user", (req, res) => {
+  res.send(req.user);
+})
+
+
+
 
 app.listen(PORT, () => console.log(`Server listening on port ${PORT}`))
---
+++ util/passportConfig.js
@@ -0,0 +1,41 @@
+const bcrypt = require("bcrypt");
+const localStrategy = require("passport-local").Strategy;
+
+module.exports = function (passport) {
+  const sqlite3 = require("sqlite3").verbose();
+  const db = new sqlite3.Database("./database/bitter.db", sqlite3.OPEN_READWRITE, (err) => {
+    if (err) return console.error(err.message);
+  })
+
+  passport.use(
+    new localStrategy((username, password, done) => {
+      db.all("SELECT * FROM users WHERE username = ?", [username.trim().toUpperCase()], (err, row) => {
+        if (err) return done(err);
+        if (row[0] === undefined) return done(null, false);
+        const user = row[0];
+
+        bcrypt.compare(password, user.password, (err, result) => {
+          if (err) return done(err);
+
+          if (result === true) {
+            return done(null, user);
+          } else {
+            return done(null, false);
+          }
+        })
+      })
+    })
+  )
+
+  passport.serializeUser((user, done) => {
+    done(null, user.id);
+  })
+
+  passport.deserializeUser((id, done) => {
+    db.all("SELECT * FROM users WHERE id = ?", [id], (err, row) => {
+      if (err) return done(err);
+      done(null, row[0])
+    })
+  })
+
+}
